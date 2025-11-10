08e69e84e81d10a6e12e89e254f83ec29696d31e
Ovidiu Gherman <govidiu15@gmail.com>
2025-11-11 00:29:52 +0200

    fixed some linter issues

--- scripts/run.py
+++ scripts/run.py
@@ -1,3 +1,5 @@
+""" This file is a wrapper that helps users run the app """
+
 import subprocess as sp
 import os
 
@@ -8,7 +10,7 @@ if os.name == 'nt':
     print("\n\n\tWindows detected\n\n")
     python3_exe = "python3.exe"
     python_exe = "python.exe"
-else: 
+else:
     print("\n\n\tLinux/MacOS detected\n\n")
 
 python_path = os.path.join(".venv", "bin", python3_exe)
@@ -17,17 +19,17 @@ if not os.path.exists(python_path):
     python_path = os.path.join(".venv", "Scripts", python_exe)
     if not os.path.exists(python_path):
         print("\n\nError please check you have a virtual enviroment set up\n\n")
-        exit(-1)
+        sys.exit(-1)
 
-main_script_path = "main.py"
-if not os.path.exists(main_script_path):
+main_path = "main.py"
+if not os.path.exists(main_path):
     print("\n\nError please check run.py script path\n\n")
-    exit(-1)
+    sys.exit(-1)
 
-if os.path.exists(python_path) and os.path.exists(main_script_path):
-    command = python_path + " " + main_script_path
+if os.path.exists(python_path) and os.path.exists(main_path):
+    command = python_path + " " + main_path
 
     sp.run(command, check=True, shell=True)
 else :
     print("\n\nUnknown error\n\n")
-    exit(-1)
+    sys.exit(-1)
