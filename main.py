4014a40a596523494077798ab62cc9a8a744ee4a
Ovidiu Gherman <govidiu15@gmail.com>
2025-11-26 15:50:14 +0200

    added test for new linter update

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 """ MAIN -> this is the entry point of the app """
-import cv2
+import cv2 as cv
+import numpy as np
+
+green = np.uint8([[[0,255,0 ]]])
+hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+print( hsv_green )
 
 print("HELL WORLD!")
