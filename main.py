3c338eb1200a069114bbe322c17e128bfd535476
Maria Mihai <maria.mi08@yahoo.com>
2025-11-26 18:09:24 +0200

    attempt at pylint fix

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """ MAIN -> this is the entry point of the app """
-import cv2 as cv
-import numpy as np
+import cv2
+import numpy
 
-green = np.uint8([[[0,255,0 ]]])
-hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+green = numpy.uint8([[[0,255,0 ]]])
+hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
 print( hsv_green )
 
 print("HELL WORLD!")
