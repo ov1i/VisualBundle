a193b41376e69f68bc827664c9483bc36ffc0805
Maria Mihai <maria.mi08@yahoo.com>
2026-01-17 17:57:45 +0200

    artistic filter module

--- main.py
+++ main.py
@@ -2,8 +2,15 @@
 import cv2
 import numpy
 from src.Llie import enhance_image
+from src.Filtering import apply_color_filter
 
-img = cv2.imread('res/city.png')
-enhanced_image = enhance_image(img)
-cv2.imwrite('res/test.png', enhanced_image)
+img = cv2.imread('res/background.png')
+#enhanced_image = enhance_image(img)
+#filtered_image = apply_color_filter(img, "Warm", 100)
+#cv2.imwrite('res/test.png', filtered_image)
+
+
+for preset in ["Warm", "Cool", "Sepia", "Cinematic", "Black & White"]:
+        out = apply_color_filter(img, preset, 100)
+        cv2.imwrite(f"res/out_{preset}.jpg", out)
 
